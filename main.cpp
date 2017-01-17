b7c75e29da0916ef0d318f1e53de120c591c7328
guralesko <gural-ang@wp.pl>
2017-01-17 20:38:23 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <windows.h>
+#include <ctime>
 
 using namespace std;
 
 void clearScreen()
 {
     system("CLS");
+}
+
+void timeSystem()
+{
+    //time on current system
+    time_t now = time(0);
+
+    // convert now to string
+    char* dt = ctime(&now);
+
+    cout << "The local date and time is: " << dt << endl;
 }
 
 string userName, userDob;
@@ -23,6 +36,8 @@ int main()
     clearScreen();
 
     cout << "Hello " << userName << endl;
+    
+    timeSystem();
     
     return 0;
 }
