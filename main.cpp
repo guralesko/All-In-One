5b109a8e715ec5af9e12f96354a8575a78a6e0b0
guralesko <gural-ang@wp.pl>
2017-01-17 21:46:06 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,25 @@
 #include <iostream>
 #include <windows.h>
 #include <ctime>
+#include <stdio.h>
+#include <conio.h>
 
 using namespace std;
 
+//function clear screan
 void clearScreen()
 {
     system("CLS");
 }
 
-void timeSystem()
-{
-    //time on current system
-    time_t now = time(0);
-
-    // convert now to string
-    char* dt = ctime(&now);
-
-    cout << "The local date and time is: " << dt << endl;
-}
+//function time on current system check at the bottom
+int timeSystem();
 
 string userName, userDob;
 
 int main()
 {
+    clearScreen();
     cout << "Hello" << endl;
     cout << "What is your name :";
     cin >> userName;
@@ -36,8 +32,42 @@ int main()
     clearScreen();
 
     cout << "Hello " << userName << endl;
-    
+
     timeSystem();
-    
+
     return 0;
+}
+
+int timeSystem()
+{
+    while( true )
+    {
+        //time on current system
+        time_t now = time(0);
+
+        // convert now to string
+        char* dt = ctime(&now);
+
+        cout << "The local date and time is: " << dt << endl;
+
+        Sleep( 1000 );
+
+        // return from time
+        char key = ' ';
+
+		if( _kbhit() )
+			key = _getch();
+
+		if( key == 13 ) //The ENTER key.
+			return main(); //Quit the while loop and return to function int main();
+
+		else if( key != ' ' )
+		{
+			cout << "You pressed: " << key << '\n';
+			key = ' ';
+            Sleep( 1000 );
+		}
+
+        clearScreen();
+    }
 }
